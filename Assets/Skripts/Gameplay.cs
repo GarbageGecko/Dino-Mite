5f6874d46a6935fdddf8a59763b194a2dd62ac2f
EbruAliz <ebrualiz@gmail.com>
2024-03-30 17:44:31 +0100

    Dino
    Dino kann sich jetzt auch nach rechts bewegen

--- Assets/Skripts/Gameplay.cs
+++ Assets/Skripts/Gameplay.cs
@@ -8,11 +8,9 @@ public class Gameplay : MonoBehaviour
     public Meteorl1 Meteor;
     public spawner Spawn;
 
-
     private bool hasmoved = true;
     private bool keyDown = false;
-    
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,31 +20,40 @@ public class Gameplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Bewegung nach links
         if (Input.GetKeyDown(KeyCode.LeftArrow) && !keyDown)
         {
-            keyDown = true; // Die Taste wurde gedrückt, sodass die Aktion nur einmal ausgeführt wird
-            // Führe hier die gewünschte Aktion aus
+            keyDown = true;
             Dino.MoveLeft();
-            hasmoved=false;
+            hasmoved = false;
         }
 
-        // Überprüfe, ob die Taste losgelassen wurde
-        if (Input.GetKeyUp(KeyCode.LeftArrow) && hasmoved==false)
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && hasmoved == false)
         {
             MeteorMovement();
-            hasmoved=true;
-            keyDown = false; // Die Taste wurde losgelassen, sodass die Aktion erneut ausgeführt werden kann
+            hasmoved = true;
+            keyDown = false;
         }
 
+        // Bewegung nach rechts
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !keyDown)
+        {
+            keyDown = true;
+            Dino.MoveRight();
+            hasmoved = false;
+        }
 
-       
+        if (Input.GetKeyUp(KeyCode.RightArrow) && hasmoved == false)
+        {
+            MeteorMovement();
+            hasmoved = true;
+            keyDown = false;
+        }
     }
 
-    public void MeteorMovement(){
+    public void MeteorMovement()
+    {
         Spawn.SpawnObject();
         Meteor.moveMeteors();
     }
-
-
 }
-
