e7ceca485334dfe721f991271b3fba5aaaf4b3fd
alinaaa11 <alinaschlecker@freenet.de>
2024-04-02 12:29:52 +0200

    Stand 02.04.2024
    GUI Menü Element, starten des Spiels mit Play Button funktioniert

---
+++ Assets/Skripts/Menu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(1);              //Load Level 1
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
